9933ae8ab6f588f9b986f5f555f134f748db5e7b
김제틴 <kimjt@MH-MacBookAir.local>
2023-11-25 18:30:32 +0900

    firebase 세팅1

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBpTKoYhnPUm4M-ZcgiB6vIXrsTeDVwxXU',
+  authDomain: 'nomad-twitter-6787c.firebaseapp.com',
+  projectId: 'nomad-twitter-6787c',
+  storageBucket: 'nomad-twitter-6787c.appspot.com',
+  messagingSenderId: '706528657892',
+  appId: '1:706528657892:web:a4bf84e665969c359487f1',
+};
+
+const app = initializeApp(firebaseConfig);
