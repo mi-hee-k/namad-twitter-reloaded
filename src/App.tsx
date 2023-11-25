bdff74c21142c4f50a5a69ac4d131c0a3f6a486d
김제틴 <kimjt@MH-MacBookAir.local>
2023-11-25 18:29:52 +0900

    firebase 세팅1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,68 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './routes/Home';
+import Profile from './routes/Profile';
+import Login from './routes/Login';
+import CreateAccount from './routes/CreateAccount';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import { useEffect, useState } from 'react';
+import LoadingScreens from './components/LoadingScreens';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/create-account',
+    element: <CreateAccount />,
+  },
+]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  *{
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+`;
+
 function App() {
-  return <></>;
+  const [isLoading, setIsLoading] = useState(true);
+  const init = async () => {
+    // wait for firebase
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    init();
+  }, []);
+
+  return (
+    <>
+      <GlobalStyles />
+      {isLoading ? <LoadingScreens /> : <RouterProvider router={router} />}
+    </>
+  );
 }
 
 export default App;
